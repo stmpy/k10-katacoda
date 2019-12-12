75994056ecf8b530e92a4d00e1828329e58660db
Travis Jeppson <stmpy@me.com>
2019-12-12 14:17:42 -0800

    changed tartup flow using single shell file, added sleep for tiller startup

--- k10-intro/assets/startup.sh
+++ k10-intro/assets/startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# wait for kubernetes to be ready
+launch.sh
+
 # once ready, install tiller
 echo "Waiting for Tiller to start..."
 kubectl create sa tiller --namespace kube-system 1> /dev/null 2>&1
@@ -6,6 +10,8 @@ kubectl create clusterrolebinding \
   --serviceaccount kube-system:tiller \
   --clusterrole cluster-admin tiller-crb 1> /dev/null 2>&1
 helm init --service-account tiller 1> /dev/null 2>&1
+
+sleep 2
 
 while [ "Running" != "$(kubectl -n kube-system get pods | grep tiller | awk {'print $3'})" ]
 do
--- k10-intro/foreground.sh
+++ k10-intro/foreground.sh
@@ -1,8 +1,4 @@
 # everything in this file is VERY verbose, removing to separate files to reduce that
 
-# wait for kubernetes
-# provided from katacoda
-launch.sh
-
 # wait for tiller
-tiller.sh
+startup.sh
