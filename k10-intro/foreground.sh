1a447979157287915c0c7ee91d1310c2d9b2d294
Travis Jeppson <stmpy@me.com>
2019-12-11 16:25:03 -0800

    added tiller launch to foreground

--- k10-intro/foreground.sh
+++ k10-intro/foreground.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# wait for kubernetes
+# provided from katacoda
 launch.sh
+
+# once ready, install tiller
+echo Waiting for Tiller to start...
+kubectl create sa tiller --namespace kube-system 1> /dev/null 2>$1
+kubectl create clusterrolebinding \
+  --serviceaccount kube-system:tiller \
+  --clusterrole cluster-admin tiller-crb 1> /dev/null 2>$1
+helm init --service-account tiller 1> /dev/null 2>$1
+
+while [ "Running" != "$(kubectl -n kube-system get pods | grep tiller | awk {'print $3'})" ]
+do
+  sleep 1
+done
+echo Tiller started
