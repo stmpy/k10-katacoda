dbff4e0edc8e77eea7dd5bab9efd68f678437cb9
Travis Jeppson <stmpy@me.com>
2019-12-11 16:59:39 -0800

    fixing dashboards, json, and tiller.sh

---
+++ k10-intro/tiller.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# once ready, install tiller
+echo "Waiting for Tiller to start..."
+kubectl create sa tiller --namespace kube-system 1> /dev/null 2>&1
+kubectl create clusterrolebinding \
+  --serviceaccount kube-system:tiller \
+  --clusterrole cluster-admin tiller-crb 1> /dev/null 2>&1
+helm init --service-account tiller 1> /dev/null 2>&1
+
+while [ "Running" != "$(kubectl -n kube-system get pods | grep tiller | awk {'print $3'})" ]
+do
+  sleep 1
+done
+echo "Tiller started"
